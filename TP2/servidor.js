f8a16dd9646b974c0cfd6a1891010abfdf4e4f9a
Joel <jsmartins2000@hotmail.com>
2022-02-28 20:09:47 +0000

    redirects feitos

--- TP2/servidor.js
+++ TP2/servidor.js
@@ -13,7 +13,7 @@ myservidor = http.createServer(function(req,res) {
     var pathname = url.parse(req.url,true).pathname
     console.log("Pathname: " + pathname)
 
-    /*
+    
     if (pathname == "/") {
         res.statusCode = 301
         res.setHeader('Location','/index.html')
@@ -27,36 +27,35 @@ myservidor = http.createServer(function(req,res) {
         res.setHeader('Location','/indexFilmes.html')
         res.end() 
     } else {
+        var parsedPath = pathname.split("/");
+        console.log(parsedPath)
+    
+        var pagina = ''
+        if (parsedPath[1] == 'index.html') {
+            pagina = "./site/index.html"
+        } else if (parsedPath[1] == 'filmes' && parsedPath[2].substring(0,1) == 'f') {
+            pagina = "./paginas/" + parsedPath[1] + "/" + parsedPath[2] + ".html"
+        } else if (parsedPath[1] == 'indexFilmes.html') {
+            pagina = "./site/indexFilmes.html"
+        } else if (parsedPath[1] == 'atores' && parsedPath[2].substring(0,1) == 'a') {
+            pagina = "./paginas/" + parsedPath[1] + "/" + parsedPath[2] + ".html"
+        } else if (parsedPath[1] == 'indexAtores.html') {
+            pagina = "./site/indexAtores.html"
+        } else if (parsedPath[2] == 'scripts.js') {
+            pagina = "./site/js/scripts.js"
+        } else if (parsedPath[2] == 'favicon.png' || parsedPath[3] == 'favicon.png') {
+            pagina = "./site/img/favicon.png"
+        } else if (parsedPath[2] == 'filme.png') {
+            pagina = "./site/img/filme.png"
+        } else if (parsedPath[2] == 'ator.png') {
+            pagina = "./site/img/ator.png"
+        } else {
+            pagina = "./paginas/404.html"
+            console.log("Pagina inexistente!")
+        }
+        console.log("Pagina a ser lida: ",pagina)
     }
-*/
-    var parsedPath = pathname.split("/");
-    console.log(parsedPath)
-
-    var pagina = ''
-    if (parsedPath[1] == 'index.html') {
-        pagina = "./site/index.html"
-    } else if (parsedPath[1] == 'filmes' && parsedPath[2].substring(0,1) == 'f') {
-        pagina = "./paginas/" + parsedPath[1] + "/" + parsedPath[2] + ".html"
-    } else if (parsedPath[1] == 'indexFilmes.html') {
-        pagina = "./site/indexFilmes.html"
-    } else if (parsedPath[1] == 'atores' && parsedPath[2].substring(0,1) == 'a') {
-        pagina = "./paginas/" + parsedPath[1] + "/" + parsedPath[2] + ".html"
-    } else if (parsedPath[1] == 'indexAtores.html') {
-        pagina = "./site/indexAtores.html"
-    } else if (parsedPath[2] == 'scripts.js') {
-        pagina = "./site/js/scripts.js"
-    } else if (parsedPath[2] == 'favicon.png' || parsedPath[3] == 'favicon.png') {
-        pagina = "./site/img/favicon.png"
-    } else if (parsedPath[2] == 'filme.png') {
-        pagina = "./site/img/filme.png"
-    } else if (parsedPath[2] == 'ator.png') {
-        pagina = "./site/img/ator.png"
-    } else {
-        console.log("Pagina inexistente!")
-    }
-
-    console.log("Pagina a ser lida: ",pagina)
-
+    
 
     // Envia a pagina html
     fs.readFile(pagina,(err,data) => {
