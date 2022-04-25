8bbc19758947efec73fad65fd245943c19199917
Joel <jsmartins2000@hotmail.com>
2022-04-25 17:02:52 +0100

    down

--- TP8/paraApp/models/para.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-var paraSchema = new mongoose.Schema({
-    data: String,
-    para: String,
-    editado: String,
-})
-
-module.exports = mongoose.model('para',paraSchema)
